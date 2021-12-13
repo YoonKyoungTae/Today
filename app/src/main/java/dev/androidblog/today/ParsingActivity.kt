9bf0488db656019ef940c15250337eee002f9590
Diordna <yoon91.dev@gmail.com>
2021-12-14 00:16:26 +0900

    git init

---
+++ app/src/main/java/dev/androidblog/today/ParsingActivity.kt
@@ -0,0 +1,120 @@
+package dev.androidblog.today
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+
+
+// todo 이미지 업로드 : https://firebase.google.com/docs/storage/android/upload-files?authuser=0
+
+
+/**
+ * 1. 데이터 파싱
+ * 2. 데이터 검증
+ * 3. 이미지 업로드
+ * 4. 업로드한 위치를 받아 DB에 저장
+ * 5. 1로 돌아감
+ */
+
+/**
+ * 1. 데이터 파싱
+ * 2. 데이터 검증
+ * 3. 데이터 업로드
+ * 4. 이후에 스토리지에 저장
+ */
+
+sealed class Components() {
+
+    abstract fun getView(): View
+
+    class Title(text: String) : Components() {
+        override fun getView(): View {
+            return TextView(context)
+        }
+    }
+
+    class Space() : Components() {
+        override fun getView(): View {
+            return TextView(context)
+        }
+    }
+
+}
+
+class ParsingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parsing)
+
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            Log.d("TEST", "START!")
+            function()
+        }
+
+
+    }
+
+
+    ////////////
+    fun add(com : Components) {
+
+    }
+
+
+
+
+    ////////////
+
+
+    private val html = "http://shortword.co.kr/web_page/detail.php?id=400&cate=0"
+    private var page = 500
+
+    private fun function() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            repeat(10) {
+                process(page)
+                page++
+                delay(500)
+                Log.d("TEST", "=================END=================")
+            }
+        }
+    }
+
+    private fun process(page: Int) {
+        val str = Jsoup.connect("http://shortword.co.kr/web_page/detail.php?id=${page}&cate=0").get()
+        val table = str.getElementById("content_table")
+        val h1 = table.select("h1")
+        val h2 = table.select("h2")
+        val img = table.select("img")[0].absUrl("src")
+
+        Log.d("TEST", "title : ${h1.text()}")
+        var index = 0
+
+        val spanP = h2.select("span")
+        spanP.forEachIndexed { i, item ->
+            val span = item.select("span")
+            Log.d("TEST", "conts : ${span.text()}")
+
+            // 줄바꿈체크
+            val next = span.next()
+            val nextnext = next.next()
+            if (next.toString() == "<br>" && nextnext.toString() == "<br>") {
+                Log.d("TEST", "=")
+            }
+
+        }
+
+        Log.d("TEST", "image : $img")
+    }
+}
