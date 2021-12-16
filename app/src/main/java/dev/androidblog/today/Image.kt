01cb66469b3040947f8dcfb6af31259ace06f3da
diordna <yoon91.dev@gmail.com>
2021-12-16 23:50:36 +0900

    Add Firebase RealTime Database Sample

---
+++ app/src/main/java/dev/androidblog/today/Image.kt
@@ -0,0 +1,7 @@
+package dev.androidblog.today
+
+data class Image(
+    val contents: String? = null,
+    val imageUrl: String? = null,
+    val date: Int? = 0
+)
--- app/src/main/java/dev/androidblog/today/ParsingActivity.kt
+++ app/src/main/java/dev/androidblog/today/ParsingActivity.kt
@@ -8,11 +8,16 @@ import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
-import timber.log.Timber
+import java.time.format.DateTimeFormatter
 
 
 // todo 이미지 업로드 : https://firebase.google.com/docs/storage/android/upload-files?authuser=0
@@ -37,27 +42,86 @@ class ParsingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Timber.plant(Timber.DebugTree())
         setContentView(R.layout.activity_parsing)
 
         val btn = findViewById<Button>(R.id.btn)
         btn.setOnClickListener {
-            Timber.d("START!")
-            function()
+            Log.d("TEST", "START!")
+            parsingFunction()
         }
+
+        val btnUpload = findViewById<Button>(R.id.btn_upload)
+        btnUpload.setOnClickListener {
+            upload()
+        }
+
+        val btn_read = findViewById<Button>(R.id.btn_read)
+        btn_read.setOnClickListener {
+            read()
+        }
+
+    }
+
+    private fun upload() {
+        val database = Firebase.database
+        val images = database.getReference("images")
+
+        lifecycleScope.launch {
+            repeat(10) { index ->
+                val image = Image(
+                    "컨텐츠 $index",
+                    "https://www.naver.com",
+                    index
+                )
+
+                images.push().setValue(image)
+            }
+        }
+
+    }
+
+    private fun read() {
+        val database = Firebase.database.reference
+        database.child("images").orderByChild("date").startAt(5.toDouble()).addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("TEST_", "onChildAdded : start")
+
+                snapshot.children.forEach {
+                    Log.d("TEST_", "onChildAdded : ${it.toString()}")
+
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("TEST_", "onChildChanged : ")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                Log.d("TEST_", "onChildRemoved : ")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("TEST_", "onChildMoved : ")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("TEST_", "onCancelled : ")
+            }
+
+        })
 
     }
 
     ////////////
     private var page = 500
 
-    private fun function() {
+    private fun parsingFunction() {
         lifecycleScope.launch(Dispatchers.IO) {
             repeat(10) {
                 process(page)
                 page++
                 delay(500)
-                Timber.d("=================END=================")
+                Log.d("TEST", "=================END=================")
             }
         }
     }
@@ -68,23 +132,23 @@ class ParsingActivity : AppCompatActivity() {
         val h2 = table.select("h2")
         val img = table.select("img")[0].absUrl("src")
 
-        Timber.d("title : " + h1.text())
+        Log.d("TEST", "title : " + h1.text())
         var index = 0
 
         val spanP = h2.select("span")
         spanP.forEachIndexed { i, item ->
             val span = item.select("span")
-            Timber.d("conts : " + span.text())
+            Log.d("TEST", "conts : " + span.text())
 
             // 줄바꿈체크
             val next = span.next()
             val nextnext = next.next()
             if (next.toString() == "<br>" && nextnext.toString() == "<br>") {
-                Timber.d("=")
+                Log.d("TEST", "=")
             }
 
         }
 
-        Timber.d("image : $img")
+        Log.d("TEST", "image : $img")
     }
 }
